1431b70599179825065c02c7c00be11a4f14cf26
Andrii <buymistrandrey@gmail.com>
2018-03-23 00:40:07 +0200

    Add generateValues function

--- tender/data_for_tender.php
+++ tender/data_for_tender.php
@@ -1,13 +1,83 @@
 <?php
 include 'tender_additional_data.php';
 
+
+function generateValues($procurement_method, $number_of_lots){
+    global $limited_procurement;
+    if (!isset($number_of_lots)){
+    $number_of_lots = 1;
+    };
+    $generated_value = rand(100000, 1000000000);
+    $currencies = ['UAH', 'USD', 'EUR', 'RUB'];  // 'GBP'
+    $currency = $currencies[rand(0, count($currencies) - 1)];
+
+    if ($procurement_method == 'esco'){
+        $value = json_decode('{"tenderValues": {
+                            "NBUdiscountRate": 0.99,
+                            "yearlyPaymentsPercentageRange": 0.8,
+                            "minimalStepPercentage": 0.02},
+                 "lotValues": {
+                            "yearlyPaymentsPercentageRange": 0.8,
+                            "minimalStepPercentage": 0.02}
+                 }', true);
+    }
+    else{
+        $value = json_decode('{"tenderValues": {
+                            "value": {
+                                "currency": currency,
+                                "amount": 0,
+                                "valueAddedTaxIncluded": True},
+
+                            "guarantee": {
+                                "currency": currency,
+                                "amount": 0
+                            },
+                            "minimalStep": {
+                                "currency": currency,
+                                "amount": "",
+                                "valueAddedTaxIncluded": True
+                            }},
+                 "lotValues": {
+                            "value": {
+                                "currency": currency,
+                                "amount": "",
+                                "valueAddedTaxIncluded": True},
+
+                            "guarantee": {
+                                "currency": currency,
+                                "amount": ""
+                            },
+                            "minimalStep": {
+                                "currency": currency,
+                                "amount": ",
+                                "valueAddedTaxIncluded": True
+                            }
+                 }}', true);
+        $value['tenderValues']['value']['amount'] = $generated_value;
+        $value['tenderValues']['guarantee']['amount'] = round(($generated_value * 0.05), 2);
+        $value['tenderValues']['minimalStep']['amount'] = round(($generated_value * 0.01), 2);
+        $value['lotValues']['value']['amount'] = round(($generated_value / $number_of_lots), 2);
+        $value['lotValues']['guarantee']['amount'] = round((($generated_value * 0.05) / $number_of_lots), 2);
+        $value['lotValues']['minimalStep']['amount'] = round((($generated_value * 0.01) / $number_of_lots), 2);
+
+        if (in_array($procurement_method, $limited_procurement, true)){
+            unset($value['tenderValues']['guarantee'], $value['tenderValues']['minimalStep'], $value['lotValues']['guarantee'], $value['lotValues']['minimalStep']);
+        };
+
+    };
+
+    return $value;
+};
+
+
 function generateTenderJson($procurement_method, $number_of_lots, $number_of_items, $accelerator, $received_tender_status, $list_of_lots_id, $if_features, $skip_auction)
 {
     global $limited_procurement;
-
+    global $negotiation_procurement;
 
     $tender_data = json_decode('{
                     "data": {
+                        "procurementMethodType": "",
                         "description": "Примечания для тендера Тест !!!!!!!!!!!",
                         "title": "TITLE!!!",
                         "status": "draft",
@@ -46,9 +116,10 @@ function generateTenderJson($procurement_method, $number_of_lots, $number_of_ite
     $tender_data['data']['procurementMethodType'] = $procurement_method;
     $tender_data['data']['procurementMethodDetails'] = 'quick, accelerator=' . $accelerator . '';
 
+    //Select submission method details
     $submission_method_details = 'quick';
     if ($skip_auction == True){
-        if ($procurement_method == $limited_procurement){
+        if (in_array($procurement_method, $limited_procurement, false)){
             if ($procurement_method == 'esco'):
                 $submission_method_details = 'quick(mode:no-auction)';
             else:
@@ -56,11 +127,23 @@ function generateTenderJson($procurement_method, $number_of_lots, $number_of_ite
             endif;
             };
     };
-
     $tender_data['data']['submissionMethodDetails'] = $submission_method_details;
+
+    //Add reason for negotiation procedures
+    if (in_array($procurement_method, $negotiation_procurement, true)){
+        $tender_data['data']['cause'] = 'noCompetition';
+        $tender_data['data']['causeDescription'] = 'Створення закупівлі для переговорної процедури за нагальною потребою';
+    };
+
+    //Add tender values
+    $values = generateValues($procurement_method, $number_of_lots);
+    foreach($values as $key => $value){
+        $tender_data['data'][$key] = $values['tenderValues'];
+    }
+
 
     return json_encode($tender_data);
 };
 
-echo generateTenderJson('METHOD', 2, 3, 1440,
+echo generateTenderJson('above', 2, 3, 1440,
 'STATUS', [1, 2], 1, true);
--- tender/tender_additional_data.php
+++ tender/tender_additional_data.php
@@ -2,26 +2,26 @@
 $above_threshold_procurement = ['aboveThresholdUA', 'aboveThresholdEU', 'aboveThresholdUA.defense', 'competitiveDialogueUA', 'competitiveDialogueEU', 'esco'];
 $below_threshold_procurement = ['belowThreshold'];
 $limited_procurement = ['reporting', 'negotiation', 'negotiation.quick'];
-//list_of_procurement_types = above_threshold_procurement + below_threshold_procurement + limited_procurement  # list of all procurement types - 1st stage only
-//without_pre_qualification_procedures = ['aboveThresholdUA', 'aboveThresholdUA.defense']
-//prequalification_procedures = ['aboveThresholdEU', 'esco']
-//competitive_procedures = ['competitiveDialogueUA', 'competitiveDialogueEU']
-//competitive_procedures_first_stage = ['competitiveDialogueUA', 'competitiveDialogueEU']
-//competitive_procedures_second_stage = ['competitiveDialogueUA.stage2', 'competitiveDialogueEU.stage2']
-//negotiation_procurement = ['negotiation', 'negotiation.quick']
-//tender_status_list = ['active.tendering', 'active.tendering.stage2', 'active.pre-qualification', 'active.pre-qualification.stage2', 'active.qualification', 'complete', 'active.enquiries', 'active', 'active.award',
-//    'active.contract']
-//without_pre_qualification_procedures_status = ['active.tendering', 'active.qualification']
-//prequalification_procedures_status = ['active.pre-qualification']
-//competitive_procedures_status = ['active.tendering.stage2', 'complete']
-//competitive_dialogue_eu_status = ['active.pre-qualification.stage2']
-//below_threshold_status = ['active.enquiries', 'active.tendering', 'active.qualification']
-//limited_status = ['active', 'active.award', 'active.contract', 'complete']
-//statuses_with_high_acceleration = ['active.tendering', 'complete', 'active.enquiries', 'active', 'active.award', 'active.contract']
-//statuses_negotiation_with_high_acceleration = ['active', 'active.award']
-//
-//# Above threshold procedures with active bid status
-//above_threshold_active_bid_procurements = ['aboveThresholdUA', 'aboveThresholdUA.defense']
+$list_of_procurement_types = $above_threshold_procurement + $below_threshold_procurement + $limited_procurement;  # list of all procurement types - 1st stage only
+$without_pre_qualification_procedures = ['aboveThresholdUA', 'aboveThresholdUA.defense'];
+$prequalification_procedures = ['aboveThresholdEU', 'esco'];
+$competitive_procedures = ['competitiveDialogueUA', 'competitiveDialogueEU'];
+$competitive_procedures_first_stage = ['competitiveDialogueUA', 'competitiveDialogueEU'];
+$competitive_procedures_second_stage = ['competitiveDialogueUA.stage2', 'competitiveDialogueEU.stage2'];
+$negotiation_procurement = ['negotiation', 'negotiation.quick'];
+$tender_status_list = ['active.tendering', 'active.tendering.stage2', 'active.pre-qualification', 'active.pre-qualification.stage2', 'active.qualification',
+                        'complete', 'active.enquiries', 'active', 'active.award', 'active.contract'];
+$without_pre_qualification_procedures_status = ['active.tendering', 'active.qualification'];
+$prequalification_procedures_status = ['active.pre-qualification'];
+$competitive_procedures_status = ['active.tendering.stage2', 'complete'];
+$competitive_dialogue_eu_status = ['active.pre-qualification.stage2'];
+$below_threshold_status = ['active.enquiries', 'active.tendering', 'active.qualification'];
+$limited_status = ['active', 'active.award', 'active.contract', 'complete'];
+$statuses_with_high_acceleration = ['active.tendering', 'complete', 'active.enquiries', 'active', 'active.award', 'active.contract'];
+$statuses_negotiation_with_high_acceleration = ['active', 'active.award'];
+
+# Above threshold procedures with active bid status
+$above_threshold_active_bid_procurements = ['aboveThresholdUA', 'aboveThresholdUA.defense'];
 //
 //# Procedure types for docs
 //documents_above_procedures = ['aboveThresholdEU', 'esco', 'aboveThresholdUA.defense', 'competitiveDialogueEU.stage2', 'aboveThresholdUA', 'competitiveDialogueUA.stage2']
